0524474099b376258f36ec48fb97e43699377908
Tom Mingarelli <tmingo0726@gmail.com>
2022-09-09 17:08:09 -0500

    more changes on checking guesses

--- src/components/game.js
+++ src/components/game.js
@@ -20,10 +20,77 @@ const Game = (props) => {
     const checkLetter = (id) => {
 
         const letter = document.getElementById(id).value;
-        const index = currentWord.indexOf(letter);
+       
+        if (!letter) {
+            //remain on this input control until a letter is entered
+            document.getElementById(id).focus();
+        }
+    }
 
-        console.log("Index is ", index);
+    const isSolved = () => {
+
+        return false;
+
+    }
+
+    const changeColor = (row) => {
+
+        switch(row) {
+            case "row1":
+                for (let i = 1; i <= 5; i++) {
+                    //I need to put the next 2 statements in a for loop to change the background color
+                    //to green for a correct letter in a correct spot and yellow for a correct letter in
+                    //a wrong spot.
+                    let letter = document.getElementById(i).value;
+                    console.log("Letter is ", letter);
+                    let index = currentWord.indexOf(letter);
+                    if (index !== -1) {
+                        if (index === (i - 1)) {
+                            console.log("The letter exists and it's in the right place");
+                            document.getElementById(index).backgroundColor = "#00FF00";
+                        } else {
+                            console.log("The letter exists but it's not in the right place");
+                            document.getElementById(index).backgroundColor = "#FFFF00";
+                        }
+                    } else {
+                        console.log("The letter does not exist");
+                    }
+                }
+                break;
+            }
+    }
+
+    const checkGuess = (row) => {
+
+        let solved = false;
+
+        switch(row) {
+            case "firstguess":
+                //First change colors of squares based on correct letters, incorrect letters, and correct letters in the right place
+                changeColor("row1");
+                //I will have to pass in a string built from the squares in the firstguess row
+                solved = isSolved();
+                if (!solved) {
+                    for (let i = 6; i <= 10; i++) {
+                        document.getElementById(i).disabled = false;
+                    }
+                }
+                break;
+            case "secondguess":
+                break;
+            case "thirdguess":
+                break;
+            case "fourthguess":
+                break;
+            case "fifthguess":
+                break;
+            case "sixthguess":
+                break;
+            default:
+                break;
+        }
         
+        /*
         if (index !== -1) {
             console.log("Index is " + index + " and i is " + id);
             if (index === (id - 1)) {
@@ -34,8 +101,7 @@ const Game = (props) => {
         } else {
             console.log("The letter does NOT exist");
             document.getElementById(id).background = "#ff0000";
-        }
-
+        }*/
 
     }
 
@@ -46,15 +112,15 @@ const Game = (props) => {
         <form className="guess-container">
             <input id="1" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("1")}></input>
             <input id="2" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("2")}></input>
-            <input id="3" type="text" className="grid-item" maxLength="1"></input>
-            <input id="4" type="text" class="grid-item" maxLength="1"></input>
-            <input id="5" type="text" class="grid-item" maxLength="1" onBlur={() => checkGuess("firstguess")}></input>
-            <input id="6" type="text" class="grid-item" maxLength="1"></input>
-            <input id="7" type="text" class="grid-item" maxLength="1"></input>
-            <input id="8" type="text" class="grid-item" maxLength="1"></input>
-            <input id="9" type="text" class="grid-item" maxLength="1"></input>
-            <input id="10" type="text" class="grid-item" maxLength="1"></input>
-            <input id="11" type="text" class="grid-item" maxLength="1"></input>
+            <input id="3" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("3")}></input>
+            <input id="4" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("4")}></input>
+            <input id="5" type="text" className="grid-item" maxLength="1" onBlur={() => checkGuess("firstguess")}></input>
+            <input id="6" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("6")}></input>
+            <input id="7" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("7")}></input>
+            <input id="8" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("8")}></input>
+            <input id="9" type="text" className="grid-item" maxLength="1" onBlur={() => checkLetter("9")}></input>
+            <input id="10" type="text" className="grid-item" maxLength="1" onBlur={() => checkGuess("secondguess")}></input>
+            <input id="11" type="text" class="grid-item" maxLength="1" ></input>
             <input id="12" type="text" class="grid-item" maxLength="1"></input>
             <input id="13" type="text" class="grid-item" maxLength="1"></input>
             <input id="14" type="text" class="grid-item" maxLength="1"></input>
